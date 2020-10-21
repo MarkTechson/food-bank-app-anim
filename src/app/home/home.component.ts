7b5d3b714975ffa089ef9e34d3042d2fedc5b55e
Mark Thompson <marktechson@gmail.com>
2020-10-21 08:19:11 -0500

    adds working animation

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,24 @@
+import { animate, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  animations: [
+    trigger('fadeIn', [
+      transition(':enter', [
+        style({
+          opacity: 0,
+          transform: 'translateY(-50px)'
+        }),
+        animate('300ms ease-out', style({
+          opacity: 1,
+          transform: 'translateY(0)'
+        }))
+      ])
+    ])
+  ]
 })
 export class HomeComponent implements OnInit {
 
